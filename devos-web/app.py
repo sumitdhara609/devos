6ce07990b370cc6c194066c70e94e669c50fa4e4
sumitdhara609 <sumitdhara609@gmail.com>
2026-04-19 18:15:49 +0530

    Fix production database tables

--- devos-web/app.py
+++ devos-web/app.py
@@ -251,8 +251,8 @@ def chart():
 # Run App
 # =========================
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == "__main__":
     app.run(debug=True)
